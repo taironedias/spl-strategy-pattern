ed92b6782e3ef25f452cd0f3007596e6eb765d0e
Tairone Dias <taironecdias@gmail.com>
2019-05-10 20:28:22 -0300

    etapa 1 - criando a arquivo de config e acessando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { QuestionDataService } from './services/question-data.service';
+import { ConfigSubjectService } from './services/config-subject.service';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [
+    QuestionDataService,
+    ConfigSubjectService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ConfigSubjectService } from '../services/config-subject.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public router: Router) {}
+  private accessPage: string;
+
+  constructor(private router: Router,
+              private configS: ConfigSubjectService) {}
+
+  ngOnInit() {
+    this.configS.getInitConfig()
+        .subscribe(data => this.accessPage = data.access);
+  }
 
   teacherUser() {
     this.router.navigate(['login']);
   }
 
   studantUser() {
-    this.router.navigate(['aluno']);
+    this.router.navigate([this.accessPage]);
   }
 
 }
